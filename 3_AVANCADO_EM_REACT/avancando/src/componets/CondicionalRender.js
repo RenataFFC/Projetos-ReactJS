35bd159ce784ab618d3baa608c632bbdb30c5cfc
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-01-09 18:03:54 -0300

    Aula React - Condicional

--- 3_AVANCADO_EM_REACT/avancando/src/componets/CondicionalRender.js
+++ 3_AVANCADO_EM_REACT/avancando/src/componets/CondicionalRender.js
@@ -1,18 +1,25 @@
-import {
-    useState
-} from "react"
+import { useState} from "react";
 
 
-const CondicionalRender = () => {
+const ConditionalRender = () => {
         const [x] = useState(false);
-
-        return ( <div >
+const[name] = useState("Joao");
+        return ( 
+            <div >
             <h1> Isso será exibido ? </h1> 
-{ x && <p> Se x for true, sim! </p>}
-{ !x && <p> Agora x é falso  </p>}
-
- </div>
+           { x && <p> Se x for true, sim! </p>}
+        { !x && <p> Agora x é falso  </p>}
+{name === "Joao" ? (
+<div>
+<p>O nome é João</p>
+</div>
+) : (
+<div>
+<p>Nome não encontrado</p>
+</div>
+)}
+            </div>
             );
         };
 
-        export default CondicionalRender;
+        export default ConditionalRender;
